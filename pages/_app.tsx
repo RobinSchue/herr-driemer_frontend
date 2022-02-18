aa4c62eb31d1258247d9cc7d6ac4fe2dd81a05c8
Robin <hello@robinschuettert.com>
2022-02-18 11:21:26 +0100

    feat: fix vercel build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { AppProps } from "next/app";
 
-interface MyAppProps extends AppProps {
-  projects: any;
-}
-
-function MyApp({ Component, pageProps, projects }: MyAppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
