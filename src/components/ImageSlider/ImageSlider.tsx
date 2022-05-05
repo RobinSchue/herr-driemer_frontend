319b451b971002dbb1d0098844e23494db7c7a4c
Robin <hello@robinschuettert.com>
2022-05-05 15:06:09 +0200

    feat: implement react-simple-image-slider

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -1,14 +1,10 @@
 /* eslint-disable react/display-name */
 /* eslint-disable @next/next/link-passhref */
-import { Box, Button, MobileStepper, Modal, useTheme } from "@mui/material";
+import { Box, Modal } from "@mui/material";
 import * as React from "react";
-import { useState } from "react";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+import SimpleImageSlider from "react-simple-image-slider";
 
 export interface ImageSliderProps {
   images: string[];
@@ -21,24 +17,6 @@ export const ImageSlider = ({
   isOpen,
   handleClose,
 }: ImageSliderProps): JSX.Element => {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = images.length;
-
-  console.log(images);
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step: number) => {
-    setActiveStep(step);
-  };
-
   const style = {
     position: "absolute" as "absolute",
     display: "flex",
@@ -52,7 +30,6 @@ export const ImageSlider = ({
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
-    p: 1,
   };
 
   return (
@@ -63,78 +40,12 @@ export const ImageSlider = ({
       aria-describedby="modal-modal-description"
     >
       <Box sx={style}>
-        {/* <Paper
-        square
-        elevation={0}
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          height: 50,
-          pl: 2,
-          bgcolor: "background.default",
-        }}
-      >
-        <Typography>{images[activeStep]}</Typography>
-      </Paper> */}
-        <AutoPlaySwipeableViews
-          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-          index={activeStep}
-          onChangeIndex={handleStepChange}
-          enableMouseEvents
-          autoPlay={false}
-        >
-          {images.map((image, index) => {
-            return (
-              <div key={image}>
-                {Math.abs(activeStep - index) <= 2 ? (
-                  <Box
-                    component="img"
-                    sx={{
-                      display: "block",
-                      overflow: "hidden",
-                      width: "100%",
-                      height: "100%",
-                    }}
-                    src={image || ""}
-                    alt={image || ""}
-                  />
-                ) : null}
-              </div>
-            );
-          })}
-        </AutoPlaySwipeableViews>
-        <MobileStepper
-          steps={maxSteps}
-          position="static"
-          activeStep={activeStep}
-          nextButton={
-            <Button
-              size="small"
-              onClick={handleNext}
-              disabled={activeStep === maxSteps - 1}
-            >
-              Next
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowLeft />
-              ) : (
-                <KeyboardArrowRight />
-              )}
-            </Button>
-          }
-          backButton={
-            <Button
-              size="small"
-              onClick={handleBack}
-              disabled={activeStep === 0}
-            >
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowRight />
-              ) : (
-                <KeyboardArrowLeft />
-              )}
-              Back
-            </Button>
-          }
+        <SimpleImageSlider
+          width={896}
+          height={504}
+          images={images}
+          showBullets={true}
+          showNavs={true}
         />
       </Box>
     </Modal>
