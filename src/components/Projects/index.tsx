1f66e949aee019f54f30872c789c71bba243c0a5
Robin <hello@robinschuettert.com>
2022-05-24 21:11:32 +0200

    fix: make imageList responsive

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,4 +1,13 @@
-import { Grid, ImageList, ImageListItem, Typography } from "@mui/material";
+import {
+  Grid,
+  Hidden,
+  ImageList,
+  ImageListItem,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 import { Project, ProjectEntity } from "../../../graphql/generated";
 import { ProjectTile } from "../ProjectTile";
@@ -8,9 +17,12 @@ export interface ProjectsProps {
 }
 
 export const Projects = ({ projects }: ProjectsProps): JSX.Element => {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("sm"));
+
   return (
     <Grid container xs={12}>
-      <ImageList variant="masonry" cols={3} gap={8}>
+      <ImageList variant="masonry" cols={isDesktop ? 3 : 1} gap={8}>
         {projects.map((project) => (
           <ImageListItem key={project.id}>
             <img
